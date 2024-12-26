4f704b61c9dbb73f016672a2c11bbb5c0e17591a
Arjun83-png <70520684+Arjun83-png@users.noreply.github.com>
2024-12-25 20:00:01 -0800

    Update wordle_game.c

--- wordle_game.c
+++ wordle_game.c
@@ -6,9 +6,6 @@
 #include <string.h>
 #include <time.h>
 
-// Once your other functions are working, please revert your main() to its
-// initial state, but please feel free to change it, during development. You'll
-// want to test out each individual function!
 int main(void) {
   char **vocabulary;
   size_t num_words;
@@ -16,17 +13,14 @@ int main(void) {
 
   srand(time(NULL));
 
-  // load up the vocabulary and store the number of words in it.
   vocabulary = load_vocabulary("vocabulary.txt", &num_words);
 
   // Randomly select one of the words from the file to be today's SECRET WORD.
   int word_index = rand() % num_words;
   char *secret = vocabulary[word_index];
 
-  // input buffer -- we'll use this to get a guess from the user.
   char guess[80];
 
-  // buffer for scoring each guess.
   char result[6] = {0};
   bool success = false;
 
