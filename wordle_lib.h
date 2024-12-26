507ee907dee40e06d49ff4c6b1b63cf70c820256
Arjun83-png <70520684+Arjun83-png@users.noreply.github.com>
2024-12-25 19:58:22 -0800

    Update wordle_lib.h

--- wordle_lib.h
+++ wordle_lib.h
@@ -11,6 +11,4 @@ char **load_vocabulary(char *filename, size_t *num_words);
 
 void free_vocabulary(char **vocabulary, size_t num_words);
 
-// Idea for the UNUSED macro from Stack Overflow user mtvec. Thanks!
-// https://stackoverflow.com/a/3599170
 #define UNUSED(x) (void)(x)
